061806848f8eb6de6dfa514f81b17ce7ba3d20ec
josephxia <15906475@qq.com>
2021-04-12 11:55:05 +0800

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+
+
+
+
+
+## 疑难问题 
+1. video自动播放
+声音无法自动播放这个在 IOS/Android 上面一直是个惯例，桌面版的 Safari 在 2017 年的 11 版本也宣布禁掉带有声音的多媒体自动播放功能，紧接着在 2018 年 4 月份发布的 Chrome 66 也正式关掉了声音自动播放，也就是说 <audio autopaly> <video autoplay>在桌面版浏览器也将失效。
+
+那么怎么解决呢？解决方式如下：
+在chrome 浏览器中输入：chrome://flags，搜索“Autoplay policy”，默认为“Default”，修改为 “No user gesture is required” 就可以了
---
+++ demo/server/index.js
@@ -0,0 +1,60 @@
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+const path = require("path");
+
+const list = ["../1.mp4", "../2.mp4"];
+let i = 1;
+
+http
+  .createServer((req, res) => {
+    console.log("req:", req.url);
+    if (req.url === "/") {
+      // res.writeHead(200, { "Content-Type": "text/html" });
+      // res.end('<video src="/v" autoplay="autoplay"></video>');
+      const html = fs.readFileSync("./index.html");
+      res.end(html);
+    }else if (req.url === '/next') {
+      i++;
+    } 
+    else if (req.url.indexOf('/v') !== -1) {
+      console.log('播放：',list[i % list.length])
+      var file = path.resolve(__dirname, list[i % list.length]);
+      fs.stat(file, function (err, stats) {
+        if (err) {
+          if (err.code === "ENOENT") {
+            // 404 Error if file not found
+            return res.sendStatus(404);
+          }
+          res.end(err);
+        }
+        var range = req.headers.range;
+        if (!range) {
+          // 416 Wrong range
+          return res.sendStatus(416);
+        }
+        var positions = range.replace(/bytes=/, "").split("-");
+        var start = parseInt(positions[0], 10);
+        var total = stats.size;
+        var end = positions[1] ? parseInt(positions[1], 10) : total - 1;
+        var chunksize = end - start + 1;
+
+        res.writeHead(206, {
+          "Content-Range": "bytes " + start + "-" + end + "/" + total,
+          "Accept-Ranges": "bytes",
+          "Content-Length": chunksize,
+          "Content-Type": "video/mp4",
+        });
+
+        var stream = fs
+          .createReadStream(file, { start: start, end: end })
+          .on("open", function () {
+            stream.pipe(res);
+          })
+          .on("error", function (err) {
+            res.end(err);
+          });
+      });
+    }
+  })
+  .listen(3001);
